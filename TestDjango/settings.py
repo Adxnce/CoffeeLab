8cd38bb30fc3b6a56576f61467bf467676872065
Adán Sánchez <adsanchez0901@gmail.com>
2025-04-28 11:51:17 -0400

    Se borró la carpeta Pianura por un error con github

--- TestDjango/settings.py
+++ TestDjango/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'coffeelab',
-    'Pianura',
+    # 'Pianura',
     'rest_framework',
     'rest_api',
     'rest_framework.authtoken',
